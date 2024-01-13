7e14344e1f638cad45ddf66ea5ac88c7b3ed11ce
Kooreis <kooreis.official@gmail.com>
2024-01-13 16:03:27 -0600

    This update adds a for loop that iterates over the array. It skips duplicate elements to ensure that the triplets are unique. It also initializes two pointers, 'j' and 'k', to the next element in the array and the last element of the array respectively.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,5 @@
-function findTriplets(arr) {
-    let triplets = [];
-    arr.sort((a, b) => a - b);
+for (let i = 0; i < arr.length - 2; i++) {
+    if (i > 0 && arr[i] === arr[i - 1]) continue;
+    let j = i + 1;
+    let k = arr.length - 1;
 }
